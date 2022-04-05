409469e79145e28913e5f9cb24f8a17c56dcb3f4
BrunoSpilak <bruno.spilak@gmail.com>
2022-04-05 19:18:50 +0200

    run processus_general.R avec correct cwd

--- Fonctions/Processus_general.R
+++ Fonctions/Processus_general.R
@@ -6,11 +6,11 @@
 
 # Répertoire de travail :
 
-setwd("~/Desktop/Motifs/")
+setwd(".")
 
 # Import des fonctions : chemin du script Fonctions_motifs.R
 
-source("~/Desktop/Motifs/Fonctions/Fonctions_motifs.R")
+source("./Fonctions/Fonctions_motifs.R")
 
 # Téléchargement du modèle d'annotation :
 
@@ -18,70 +18,72 @@ source("~/Desktop/Motifs/Fonctions/Fonctions_motifs.R")
 # dl <- udpipe_download_model(language = "french")
 
 # Annotation : 
-annotation_udpipe(path = "~/Motifs/Corpus_test_motifs/", 
-                  model = "~/Motifs/model_udpipe/french-gsd-ud-2.5-191206.udpipe")
-
+annotation_udpipe(path = "./Corpus-torun//", 
+                  model = "./model_udpipe/french-gsd-ud-2.5-191206.udpipe")
+# => create UDPipe_corpus_complet.csv
 
 # Transformation en motifs UDPipe
-regex_corpus_entier_UDPipe(path = "~/Desktop/Motifs/", 
-                    corpus = "UDPipe_corpus_complet.csv")
+regex_corpus_entier_UDPipe(path = "./", 
+                           corpus = "./UDPipe_corpus_complet.csv")
+
+# Si nouvelle analyse avec different n gram reprendre d ici
 
 # Choix du nombre de ngrams : 
-choix_nb_ngrams(path = "~/Desktop/Motifs/", csv = "Corpus_motifs_UDPipe.csv")
+choix_nb_ngrams(path = "./", csv = "./Corpus_motifs_UDPipe.csv")
+# create corpus_motifs_grams.csv
 
 # Wordcloud : 
-
-motifs_nuage(path = "~/Desktop/Motifs/", 
-             csv = "Corpus_motifs_UDPipe.csv", 
-             nmots = 70)
+motifs_nuage(path = "./", 
+             csv = "./corpus_motifs_grams.csv", 
+             nmots = 10)
 
 # Histogrammes : 
-
-motifs_histograms(path = "~/Desktop/Motifs/", 
+motifs_histograms(path = "./", 
                   csv = "corpus_motifs_grams.csv", 
-                  nmots = 30)
+                  nmots = 10)
 
 # TF-IDF :
-tf_idf_motifs(path = "~/Desktop/Motifs/",
-              csv = "corpus_motifs_grams.csv", nombre_motifs = 20)
+tf_idf_motifs(path =  "./", 
+              csv = "corpus_motifs_grams.csv", nombre_motifs = 10)
 
 # ACP :
-motifs_acp(path = "~/Desktop/Motifs/", csv = "corpus_motifs_grams.csv", 
-           freq_filter = 50, n_obs = "all")
+motifs_acp(path = "./", csv = "corpus_motifs_grams.csv", 
+           freq_filter = 3, n_obs = "all")
 
 
 # Spécificités :
-calcul_de_specificites(path = "~/Desktop/Motifs/",
+calcul_de_specificites(path = "./",
                        csv = "corpus_motifs_grams.csv",
                        retrait_frequence_1 = TRUE)
 
 # Densité :
-motifs_densite(path = "~/Desktop/Motifs/", 
-               csv = "corpus_motifs_grams.csv", 
-               filtre = "Flaubert-Bovary.txt-ut8.txt", 
-               motif1 = "le NC de le NC",
-               motif2 = "NC de le NC ,",
-               motif3 = "le NC de DETPOSS NC",
-               motif4 = "à le NC de le",
-               motif5 = "NC de le NC .",
-               bd = 4000,
-               titre_graphique = "Densité sur cinq motifs - Madame Bovary")
+# motifs_densite(path = "~/Desktop/Motifs/", 
+#                csv = "corpus_motifs_grams.csv", 
+#                filtre = "Flaubert-Bovary.txt-ut8.txt", 
+#                motif1 = "le NC de le NC",
+#                motif2 = "NC de le NC ,",
+#                motif3 = "le NC de DETPOSS NC",
+#                motif4 = "à le NC de le",
+#                motif5 = "NC de le NC .",
+#                bd = 4000,
+#                titre_graphique = "Densité sur cinq motifs - Madame Bovary")
 
 # Statistiques générales :
-stats_motifs(path = "~/Desktop/Motifs/", 
-             csv = "corpus_motifs_grams.csv")
+# stats_motifs(path = ".", 
+#              csv = "corpus_motifs_grams.csv")
 
 # Retour aux textes : 
-retour_texte_specificites(path = "~/Desktop/Motifs/",
+retour_texte_specificites(path = ".",
                           csv_corpus_motifs = "corpus_motifs_grams.csv",
                           csv_corpus_specificites = "Corpus_spec_freq.csv", 
-                          frequence = 50)
+                          frequence = 3)
+# Quelle longueur a votre motif => n_gram defini au dessus
 
 # Retour aux textes à partir d'un motif :
-retour_texte_specificites_un_motif(path = "~/Desktop/Motifs/",
+retour_texte_specificites_un_motif(path = ".",
                                    csv_corpus_motifs = "corpus_motifs_grams.csv",
                                    csv_corpus_specificites = "Corpus_spec_freq.csv",
-                                   motif_cible = "de le NC de le")
+                                   motif_cible = ", à le NC")
 
 
 
