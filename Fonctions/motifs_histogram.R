d8c191a14ccb93711a9ad0ad2ef99baf65ab64f7
BrunoSpilak <bruno.spilak@gmail.com>
2022-04-06 22:26:52 +0200

    motifs_histogram operationelle

--- Fonctions/motifs_histogram.R
+++ Fonctions/motifs_histogram.R
@@ -1,43 +1,66 @@
-#' Titre : Scripts motifs - Histogramme
-#' Auteurs : Dominique Legallois, Antoine de Sacy
-#' Date: 6 octobre 2021.
+#' Histogramme des motifs
+#'
+#' Fonction génération d'histogramme
+#'
+#' @param corpus_grams data.frame sous format mots || motifs || Oeuvre
+#'
+#' @param corpus_path string chemin du csv contenant les motifs en ngram
+#'
+#' @param nmots int sélection du nombre de motifs à afficher
+#'
+#' @param freq string "rel" pour fréquence relative ou "abs" pour fréquence absolue
 
-## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
-
-# Entrée : un corpus sous format : mots || motifs || Oeuvre
-
-## Paramètres :
-
-# path = chemin, préférablement celui où se trouve le csv.
-# csv = "corpus_motifs_grams.csv"==> Sortie du script choix de ngrams.
-# nmots = 55 ==> sélection du nombre de motifs à afficher dans la visualisation.
-
-## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
-
-motifs_histogram <- function(path = "~/Dropbox/2020-2021/Motifs/",
-                             csv = "corpus_motifs_grams.csv",
-                             nmots = 25) {
-  # Librairies :
-  
-  require("dplyr")
-  require("tidytext")
-  require("tidyverse")
-  require("RColorBrewer")
-  require("reshape2")
-  require("ggsci")
-  require("data.table")
-  require("ggpubr")
+#' @example
+#' motifs_histogram(corpus_path="corpus_motifs_grams.csv", nmots = 25)
+#'
+#' @export
+motifs_histogram <- function(corpus_grams = NULL,
+                             corpus_path = NULL,
+                             nmots = 25,
+                             freq = "rel") {
+  # require("dplyr")
+  # require("tidytext")
+  # require("tidyverse")
+  # require("RColorBrewer")
+  # require("reshape2")
+  # require("ggsci")
+  # require("data.table")
+  # require("ggpubr")
   
   # Lecture des données :
-  
-  setwd(path)
-  corpus_grams <-
-    fread(
-      csv,
-      encoding = "UTF-8",
-      header = TRUE,
-      stringsAsFactors = FALSE
-    )
+  if (is.null(corpus_grams) & is.null(corpus_path)) {
+    corpus_path = file.path(OUTPUT_DIR, "corpus_motifs_grams.csv")
+    message("Chargement du corpus_grams depuis le fichier ", corpus_path)
+    if (file.exists(corpus_path)) {
+      corpus_grams <-
+        data.table::fread(
+          corpus_path,
+          encoding = "UTF-8",
+          header = TRUE,
+          stringsAsFactors = FALSE
+        )
+    } else {
+      stop("Le fichier ", corpus_path, " n'existe pas.")
+    }
+  } else if (is.null(corpus_grams) & (!is.null(corpus_path))) {
+    message("Chargement du corpus_grams depuis le fichier ", corpus_path)
+    if (file.exists(corpus_path)) {
+      corpus_grams <-
+        data.table::fread(
+          corpus_path,
+          encoding = "UTF-8",
+          header = TRUE,
+          stringsAsFactors = FALSE
+        )
+    } else {
+      stop("Le fichier ", corpus_path, " n'existe pas.")
+    }
+  } else if (!is.null(corpus_grams) & (is.null(corpus_path))) {
+    # nothing to do
+  } else {
+    stopifnot(!is.null(corpus_grams) & (!is.null(corpus_path)))
+    stop("Vous ne pouvez pas passer à la fois 'corpus_grams' et 'corpus_path' en argument.")
+  }
   
   # Vérification okazou :
   corpus_grams <- corpus_grams[, c("mots", "motifs", "Oeuvre")]
@@ -79,58 +102,36 @@ motifs_histogram <- function(path = "~/Dropbox/2020-2021/Motifs/",
     factor(corpus_words_ngrams$motifs,
            levels = unique(corpus_words_ngrams$motifs))
   
-  ## Fréquences absolues :
-  
-  plot_abs <- ggplot(data = corpus_words_ngrams[1:nmots, ],
-                     aes(x = motifs, y = n, fill = Oeuvre)) +
-    geom_histogram(stat = "identity",
-                   position = "dodge",
-                   show.legend = T) +
-    theme_minimal() +
-    theme(legend.position = "top")
-  
-  plot_abs <-
-    plot_abs + theme(axis.text.x = element_text(angle = 50, hjust = 1))
-  
-  ## Fréquences relatives
-  
-  # Réordonnancement de la df par fréquences relatives :
-  
-  df_freq_rel <- corpus_words_ngrams %>%
-    arrange(-rel_freq)
-  
-  # Visualisation :
-  
-  plot_freq <- ggplot(data = df_freq_rel[1:nmots, ],
-                      aes(x = motifs, y = rel_freq, fill = Oeuvre)) +
-    geom_histogram(stat = "identity",
-                   position = "dodge",
-                   show.legend = T) +
-    theme_minimal() +
-    theme(legend.position = "top")
-  
-  plot_freq <-
-    plot_freq + theme(axis.text.x = element_text(angle = 50, hjust = 1))
-  
-  freq_rel <-
-    as.numeric(
-      readline(
-        "Fréquences relatives, tapez 1 et enter \n Fréquences absolues, tapez 2 et enter"
-      )
-    )
-  
-  if (freq_rel == 1) {
-    return(plot_freq)
+  if (freq == "rel") {
+    ## Fréquences relatives
+    # Réordonnancement de la df par fréquences relatives :
+    df_freq_rel <- corpus_words_ngrams %>%
+      arrange(-rel_freq)
+    # Visualisation :
+    hist_plot <- ggplot2::ggplot(data = df_freq_rel[1:nmots, ],
+                                 ggplot2::aes(x = motifs, y = rel_freq, fill = Oeuvre)) +
+      ggplot2::geom_histogram(stat = "identity",
+                              position = "dodge",
+                              show.legend = T) +
+      ggplot2::theme_minimal() +
+      ggplot2::theme(legend.position = "top")
     
+    hist_plot <-
+      hist_plot + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 50, hjust = 1))
+
+  } else if (freq == "abs") {
+    ## Fréquences absolues :
+    hist_plot <- ggplot2::ggplot(data = corpus_words_ngrams[1:nmots, ],
+                                ggplot2::aes(x = motifs, y = n, fill = Oeuvre)) +
+      ggplot2::geom_histogram(stat = "identity",
+                              position = "dodge",
+                              show.legend = T) +
+      ggplot2::theme_minimal() +
+      ggplot2::theme(legend.position = "top")
+    hist_plot <-
+      hist_plot + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 50, hjust = 1))
+  } else{
+    stop("L'argument freq=", freq, " n'est pas valide...")
   }
-  
-  
-  if (freq_rel == 2) {
-    return(plot_abs)
-  }
-  
-  else{
-    print("Votre choix ne correspond pas aux critères binaires proposés...!")
-  }
-  
+  return (hist_plot)
 }
