dc841fd0dc63cb8527c7facbc2e9bb26e71db302
BrunoSpilak <bruno.spilak@gmail.com>
2022-04-06 18:27:03 +0200

    create tag_motif_pipeline.R

---
+++ R/tag_motif_pipeline.R
@@ -0,0 +1,30 @@
+#' Pipeline Motifs UDPipe
+#'
+#' Étiquetage du corpus et transformation en motifs UDPpipe
+#'
+#' @param path string: Chemin du dossier contenant les différents corpus.
+#' 
+#' @param corpus string: Chemin du dossier contenant les différents corpus.
+#'
+#' @param save_output boolean: Sauvegarde les résultats
+#' 
+#' @param overwrite boolean: Écrase et sauve de nouveaux les résultats
+#'
+#'
+#' @return DataFrame: corpus_annote avec les columns (mots || lemmes || POS || feats || Oeuvre)
+#'
+#' @example
+#' corpus_annote <- annotation_udpipe("curpus-test")
+#'
+#' @export
+
+
+source("./Fonctions/regex_corpus_UDPipe.R")
+source("./Fonctions/annotation_udpipe.R")
+tag_motif_pipeline <- function(path, corpus = NULL, save_output = TRUE, overwrite = FALSE){
+  corpus_annote = annotation_udpipe(path = "./Corpus-torun", save_output = save_output, overwrite = overwrite)
+  corpus_motifs = regex_corpus_UDPipe(corpus = corpus_annote, save_output = save_output, overwrite = overwrite)
+  
+  return (corpus_motifs)
+}
+  
