a8cd2d43fd2b23e421748cf3a6f7a03afd4fc726
BrunoSpilak <bruno.spilak@gmail.com>
2022-04-06 23:14:03 +0200

    adding package relative imports in motifs_tf_idf.R

--- Fonctions/motifs_tf_idf.R
+++ Fonctions/motifs_tf_idf.R
@@ -21,8 +21,39 @@ motifs_tf_idf <- function(corpus_grams=NULL,
                           save_output = FALSE,
                           overwrite=FALSE){
   # Lecture des données :
-  corpus_grams <- fread(corpus_path, encoding = "UTF-8", 
-                        header = TRUE, stringsAsFactors = FALSE)
+  if (is.null(corpus_grams) & is.null(corpus_path)) {
+    corpus_path = file.path(OUTPUT_DIR, "corpus_motifs_grams.csv")
+    message("Chargement du corpus_grams depuis le fichier ", corpus_path)
+    if (file.exists(corpus_path)) {
+      corpus_grams <-
+        data.table::fread(
+          corpus_path,
+          encoding = "UTF-8",
+          header = TRUE,
+          stringsAsFactors = FALSE
+        )
+    } else {
+      stop("Le fichier ", corpus_path, " n'existe pas.")
+    }
+  } else if (is.null(corpus_grams) & (!is.null(corpus_path))) {
+    message("Chargement du corpus_grams depuis le fichier ", corpus_path)
+    if (file.exists(corpus_path)) {
+      corpus_grams <-
+        data.table::fread(
+          corpus_path,
+          encoding = "UTF-8",
+          header = TRUE,
+          stringsAsFactors = FALSE
+        )
+    } else {
+      stop("Le fichier ", corpus_path, " n'existe pas.")
+    }
+  } else if (!is.null(corpus_grams) & (is.null(corpus_path))) {
+    # nothing to do
+  } else {
+    stopifnot(!is.null(corpus_grams) & (!is.null(corpus_path)))
+    stop("Vous ne pouvez pas passer à la fois 'corpus_grams' et 'corpus_path' en argument.")
+  }
   
   # Vérification okazou :
   corpus_grams <- corpus_grams[,c("mots", "motifs", "Oeuvre")]
@@ -33,8 +64,8 @@ motifs_tf_idf <- function(corpus_grams=NULL,
   
   ## Dénombrement + filtrage éventuel des données : ex : n > 10
   corpus_words_ngrams <- corpus_grams %>%
-    count(Oeuvre, motifs, sort = TRUE) %>%
-    filter(n > 1)
+    dplyr::count(Oeuvre, motifs, sort = TRUE) %>%
+    dplyr::filter(n > 1)
   
   ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  
   
@@ -43,51 +74,51 @@ motifs_tf_idf <- function(corpus_grams=NULL,
   ## Ajout d'une colonne total words pour normaliser la fréquence (fréquence relative) :
   
   total_words <- corpus_words_ngrams %>%
-    group_by(Oeuvre) %>%
-    summarize(total = sum(n))
+    dplyr::group_by(Oeuvre) %>%
+    dplyr::summarize(total = sum(n))
   
-  corpus_words_ngrams <- left_join(corpus_words_ngrams, total_words) 
+  corpus_words_ngrams <- dplyr::left_join(corpus_words_ngrams, total_words) 
   
   corpus_words_ngrams <- corpus_words_ngrams %>%
-    bind_tf_idf(motifs, Oeuvre, n)
+    tidytext::bind_tf_idf(motifs, Oeuvre, n)
   
   corpus_words_ngrams %>%
-    select(-total) %>%
-    arrange(desc(tf_idf))
+    dplyr::select(-total) %>%
+    dplyr::arrange(desc(tf_idf))
   
   tf_idf_export <- corpus_words_ngrams %>%
-    select(-total) %>%
-    arrange(desc(tf_idf))
+    dplyr::select(-total) %>%
+    dplyr::arrange(desc(tf_idf))
   
   # Visualisation :
   if(plot_type == "sep"){
     tf_idf_grid <- corpus_words_ngrams %>%
-      select(-total) %>%
-      arrange(desc(tf_idf)) %>%
-      mutate(motifs = factor(motifs, levels = rev(unique(motifs)))) %>% 
-      group_by(Oeuvre) %>% 
-      top_n(n_motifs) %>% # À moduler suivant les besoins : > x ; == x ; etc.
-      ungroup %>%
-      ggplot(aes(motifs, tf_idf, fill = Oeuvre)) +
-      geom_col(show.legend = T) +
-      labs(x = NULL, y = "TF-IDF") +
-      facet_wrap(~Oeuvre, ncol = 2, scales = "free") +
-      coord_flip() +
-      theme_minimal()
+      dplyr::select(-total) %>%
+      dplyr::arrange(dplyr::desc(tf_idf)) %>%
+      dplyr::mutate(motifs = factor(motifs, levels = rev(unique(motifs)))) %>% 
+      dplyr::group_by(Oeuvre) %>% 
+      dplyr::top_n(n_motifs) %>% # À moduler suivant les besoins : > x ; == x ; etc.
+      dplyr::ungroup %>%
+      ggplot2::ggplot(ggplot2::aes(motifs, tf_idf, fill = Oeuvre)) +
+      ggplot2::geom_col(show.legend = T) +
+      ggplot2::labs(x = NULL, y = "TF-IDF") +
+      ggplot2::facet_wrap(~Oeuvre, ncol = 2, scales = "free") +
+      ggplot2::coord_flip() +
+      ggplot2::theme_minimal()
     return(tf_idf_grid)
   } else if(plot_type == "group"){
     tf_idf_all <- corpus_words_ngrams %>% 
-      select(-total) %>%
-      arrange(desc(tf_idf)) %>%
-      mutate(motifs = factor(motifs, levels = rev(unique(motifs)))) %>% 
-      top_n(n_motifs) %>% # À moduler suivant les besoins : > x ; == x ; etc.
-      ungroup() %>%
-      mutate(word = reorder(motifs, tf_idf)) %>%
-      ggplot(aes(motifs, tf_idf, fill = Oeuvre)) +
-      geom_bar(stat = "identity") +
-      ylab("TF-IDF") +
-      coord_flip() +
-      theme_minimal()
+      dplyr::select(-total) %>%
+      dplyr::arrange(dplyr::desc(tf_idf)) %>%
+      dplyr::mutate(motifs = factor(motifs, levels = rev(unique(motifs)))) %>% 
+      dplyr::top_n(n_motifs) %>% # À moduler suivant les besoins : > x ; == x ; etc.
+      dplyr::ungroup() %>%
+      dplyr::mutate(word = stats::reorder(motifs, tf_idf)) %>%
+      ggplot2::ggplot(ggplot2::aes(motifs, tf_idf, fill = Oeuvre)) +
+      ggplot2::geom_bar(stat = "identity") +
+      ggplot2::ylab("TF-IDF") +
+      ggplot2::coord_flip() +
+      ggplot2::theme_minimal()
     return(tf_idf_all)
   } else{
     stop("L'argument plot_type=", plot_type, " n'est pas valide...")
