996494026ac06f1fea266caf0a142d24ed8de7c5
BrunoSpilak <bruno.spilak@gmail.com>
2022-04-06 21:17:20 +0200

    auto-format motifs_nuage.R

--- Fonctions/motifs_nuage.R
+++ Fonctions/motifs_nuage.R
@@ -5,105 +5,131 @@
 #' @param corpus data.frame sous format mots || motifs || Oeuvre
 #'
 #' @param corpus_path string chemin du csv contenant les motifs en ngram
-#' 
+#'
 #' @param nmots int sélection du nombre de motifs à afficher
 #'
 #' @param freq string "rel" pour fréquence relative ou "abs" pour fréquence absolue
-#' 
+#'
 #' @example
 #' corpus_annote <- motifs_nuage(corpus_path="corpus_motifs_grams.csv", nmots = 25)
 #'
 #' @export
-motifs_nuage <- function(corpus = NULL, corpus_path = NULL, nmots = 25, freq = "rel"){
-  
-  # Librairies :
-  
-  # require("dplyr")
-  # require("tidytext")
-  # require("tidyverse")
-  # require("ggwordcloud")
-  # require("RColorBrewer")
-  # require("reshape2")
-  # require("ggsci")
-  # require("data.table")
-  
-  # Lecture des données :
-  if (is.null(corpus) & is.null(corpus_path)) {
-    corpus_path = file.path(OUTPUT_DIR, "corpus_motifs_grams.csv")
-    message("Chargement du corpus depuis le fichier ", corpus_path)
-    if (file.exists(corpus_path)) {
-      corpus <- data.table::fread(corpus_path, encoding = "UTF-8", header = TRUE, stringsAsFactors = FALSE)
+motifs_nuage <-
+  function(corpus = NULL,
+           corpus_path = NULL,
+           nmots = 25,
+           freq = "rel") {
+    # Librairies :
+    
+    # require("dplyr")
+    # require("tidytext")
+    # require("tidyverse")
+    # require("ggwordcloud")
+    # require("RColorBrewer")
+    # require("reshape2")
+    # require("ggsci")
+    # require("data.table")
+    
+    # Lecture des données :
+    if (is.null(corpus) & is.null(corpus_path)) {
+      corpus_path = file.path(OUTPUT_DIR, "corpus_motifs_grams.csv")
+      message("Chargement du corpus depuis le fichier ", corpus_path)
+      if (file.exists(corpus_path)) {
+        corpus <-
+          data.table::fread(
+            corpus_path,
+            encoding = "UTF-8",
+            header = TRUE,
+            stringsAsFactors = FALSE
+          )
+      } else {
+        stop("Le fichier ", corpus_path, " n'existe pas.")
+      }
+    } else if (is.null(corpus) & (!is.null(corpus_path))) {
+      message("Chargement du corpus depuis le fichier ", corpus_path)
+      if (file.exists(corpus_path)) {
+        corpus <-
+          data.table::fread(
+            corpus_path,
+            encoding = "UTF-8",
+            header = TRUE,
+            stringsAsFactors = FALSE
+          )
+      } else {
+        stop("Le fichier ", corpus_path, " n'existe pas.")
+      }
+    } else if (!is.null(corpus) & (is.null(corpus_path))) {
+      # nothing to do
     } else {
-      stop("Le fichier ", corpus_path, " n'existe pas.")
+      stopifnot(!is.null(corpus) & (!is.null(corpus_path)))
+      stop("Vous ne pouvez pas passer à la fois 'corpus' et 'corpus_path' en argument.")
     }
-  } else if (is.null(corpus) & (!is.null(corpus_path))) {
-    message("Chargement du corpus depuis le fichier ", corpus_path)
-    if (file.exists(corpus_path)) {
-      corpus <- data.table::fread(corpus_path, encoding = "UTF-8", header = TRUE, stringsAsFactors = FALSE)
+    
+    # Vérification okazou (pb index) :
+    corpus <- corpus[, c("mots", "motifs", "Oeuvre")]
+    
+    ## Retrait des cases vides :
+    corpus <- corpus[complete.cases(corpus), ]
+    
+    ## Dénombrement + filtrage éventuel des données : ex : n > 10
+    corpus <- corpus %>%
+      count(Oeuvre, motifs, sort = TRUE)
+    
+    ## Ajout d'une colonne total words pour normaliser la fréquence (fréquence relative) :
+    
+    total_words <- corpus %>%
+      group_by(Oeuvre) %>%
+      summarize(total = sum(n))
+    
+    corpus_words_ngrams <-
+      left_join(corpus, total_words, by = "Oeuvre")
+    
+    ## Calcul de la fréquence relative :
+    
+    corpus_words_ngrams$rel_freq <-
+      corpus_words_ngrams$n / corpus_words_ngrams$total
+    
+    # Ordonnancement par fréquences relatives :
+    corpus_words_ngrams <-
+      corpus_words_ngrams[order(corpus_words_ngrams$rel_freq, decreasing = T), ]
+    
+    if (freq == "abs") {
+      ## Visualisation sur les fréquences absolues :
+      world_cloud_plot <- ggplot2::ggplot(
+        corpus[1:nmots, ],
+        # TOdo : changer 50 par une variable dans la fonction
+        ggplot2::aes(
+          label = motifs,
+          size = n,
+          x = Oeuvre,
+          color = Oeuvre,
+          fill = Oeuvre
+        )
+      ) +
+        ggwordcloud::geom_text_wordcloud_area(shape = "diamond") +
+        ggplot2::scale_size_area(max_size = 15) +
+        ggplot2::scale_x_discrete() +
+        ggplot2::theme_minimal()
+    } else if (freq == "rel") {
+      ## Visualisation sur les fréquences relatives :
+      world_cloud_plot <- ggplot2::ggplot(
+        corpus_words_ngrams[1:nmots, ],
+        # Choix du nombre de motifs à faire apparaître
+        ggplot2::aes(
+          label = motifs,
+          size = rel_freq,
+          x = Oeuvre,
+          color = Oeuvre,
+          fill = Oeuvre
+        )
+      ) +
+        ggwordcloud::geom_text_wordcloud_area(shape = "diamond") +
+        ggplot2::scale_size_area(max_size = 15) + # à moduler suivant le nb de motifs
+        ggplot2::scale_x_discrete() +
+        ggplot2::theme_minimal()
     } else {
-      stop("Le fichier ", corpus_path, " n'existe pas.")
+      stop("L'argument freq=", freq, " n'est pas valide...")
     }
-  } else if (!is.null(corpus) & (is.null(corpus_path))) {
-    # nothing to do
-  } else {
-    stopifnot(!is.null(corpus) & (!is.null(corpus_path)))
-    stop("Vous ne pouvez pas passer à la fois 'corpus' et 'corpus_path' en argument.")
+    return(world_cloud_plot)
+    
   }
-  
-  # Vérification okazou (pb index) :
-  corpus <- corpus[,c("mots", "motifs", "Oeuvre")]
-  
-  ## Retrait des cases vides :
-  corpus <- corpus[complete.cases(corpus),]
-  
-  ## Dénombrement + filtrage éventuel des données : ex : n > 10
-  corpus <- corpus %>%
-    count(Oeuvre, motifs, sort = TRUE)
-  
-  ## Ajout d'une colonne total words pour normaliser la fréquence (fréquence relative) :
-  
-  total_words <- corpus %>%
-    group_by(Oeuvre) %>%
-    summarize(total = sum(n))
-  
-  corpus_words_ngrams <- left_join(corpus, total_words, by = "Oeuvre") 
-  
-  ## Calcul de la fréquence relative :
-  
-  corpus_words_ngrams$rel_freq <- corpus_words_ngrams$n / corpus_words_ngrams$total
-  
-  # Ordonnancement par fréquences relatives :
-  corpus_words_ngrams <- corpus_words_ngrams[order(corpus_words_ngrams$rel_freq, decreasing = T),] 
-  
-  if (freq == "abs"){
-    ## Visualisation sur les fréquences absolues :
-    world_cloud_plot <- ggplot2::ggplot(
-      corpus[1:nmots,], # TOdo : changer 50 par une variable dans la fonction
-      ggplot2::aes(
-        label = motifs, size = n,
-        x = Oeuvre, color = Oeuvre, fill = Oeuvre 
-      )
-    ) +
-      ggwordcloud::geom_text_wordcloud_area(shape = "diamond") +
-      ggplot2::scale_size_area(max_size = 15) +
-      ggplot2::scale_x_discrete() +
-      ggplot2::theme_minimal()
-  } else if (freq == "rel") {
-    ## Visualisation sur les fréquences relatives :
-    world_cloud_plot <- ggplot2::ggplot(
-      corpus_words_ngrams[1:nmots,], # Choix du nombre de motifs à faire apparaître
-      ggplot2::aes(
-        label = motifs, size = rel_freq,
-        x = Oeuvre, color = Oeuvre, fill = Oeuvre 
-      )
-    ) +
-      ggwordcloud::geom_text_wordcloud_area(shape = "diamond") +
-      ggplot2::scale_size_area(max_size = 15) + # à moduler suivant le nb de motifs
-      ggplot2::scale_x_discrete() +
-      ggplot2::theme_minimal()
-  } else {
-    stop("L'argument freq=", freq," n'est pas valide...")
-  }
-  return(world_cloud_plot)
-  
-}
